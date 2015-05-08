f90b8c7ccecc1ebc99c7487973e7269600c436b7
Christopher Schmidt <pecos@sqleb.de>
2015-05-08 20:18:56 +0200

    Measuring performance

--- src/count.py
+++ src/count.py
@@ -1,10 +1,15 @@
 import slate
 import string
+import time
+
+iterations = 0
 
 ''' Counting function '''
 def count_words(word, word_list):
+    global iterations
     s = word.lower().translate(string.maketrans("", ""), string.punctuation)
     for key in word_list:
+        iterations += 1
         if key.lower() == s:
             word_list[key] += 1
             break
@@ -26,13 +31,16 @@ with open('../text.pdf') as file:
     document = slate.PDF(file)
 
 ''' Iterate through data '''
+
+time_start = time.time()
+
 for page in document:
     words = page.split(" ")
     for word in words:
         count_words(word, words_positive)
         count_words(word, words_negative)
 
-
+time_end = time.time()
 
 ''' Print the results '''
 
@@ -66,3 +74,4 @@ print "-------------------------------"
 
 
 print "Total: postive = %s, negative = %s" % (count_positive, count_negative)
+print "Processing finished after %s iterations (time: %s)" % (iterations, time_end - time_start)

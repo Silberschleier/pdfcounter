f64ec0f15bf3e9797f45f330c88aff1e76c52642
Christopher Schmidt <pecos@sqleb.de>
2015-05-08 14:34:56 +0200

    Switched from list of tuples to dictionaries

--- src/count.py
+++ src/count.py
@@ -1,22 +1,28 @@
 import slate
 
-words_positive = []
-words_negative = []
+''' Each wordlist contains tuples containing the words and the number of appearances '''
+words_positive = {}
+words_negative = {}
 
+''' Reading the wordlists and initialize each counter with zero '''
 file_positive = open("../wordlist_positive.txt")
 for line in file_positive:
-    words_positive.append((line.rstrip('\n'), 0))
+    words_positive[line.rstrip('\n')] = 0
 
 file_negative = open("../wordlist_negative.txt")
 for line in file_negative:
-    words_negative.append((line.rstrip('\n'), 0))
+    words_negative[line.rstrip('\n')] = 0
 
-
-print words_positive
-print words_negative
 
 with open('../text.pdf') as file:
     document = slate.PDF(file)
 
+
 for page in document:
     words = page.split(" ")
+    for word in words:
+        for i in words_negative:
+            if i.lower() in word.lower():
+                words_negative[i] = words_negative[i] + 1
+
+print words_negative

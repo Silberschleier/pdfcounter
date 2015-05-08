78082328de4d58c539c82eccaba7e770065a61da
Christopher Schmidt <pecos@sqleb.de>
2015-05-08 14:50:53 +0200

    Counting completed

--- src/count.py
+++ src/count.py
@@ -1,6 +1,11 @@
 import slate
 
-''' Each wordlist contains tuples containing the words and the number of appearances '''
+
+def contains_word(word, word_list):
+    for key in word_list:
+            if key.lower() in word.lower():
+                word_list[key] += 1
+
 words_positive = {}
 words_negative = {}
 
@@ -21,8 +26,25 @@ with open('../text.pdf') as file:
 for page in document:
     words = page.split(" ")
     for word in words:
-        for i in words_negative:
-            if i.lower() in word.lower():
-                words_negative[i] = words_negative[i] + 1
+        contains_word(word, words_negative)
+        contains_word(word, words_positive)
 
-print words_negative
+
+count_negative = 0
+count_positive = 0
+
+print "Positive:"
+print "---------"
+for key, value in sorted(words_positive.iteritems(), key=lambda (k, v): (v, k), reverse=True):
+    count_positive += value
+    print "%s: %s" % (key, value)
+
+
+print "Negative:"
+print "---------------"
+for key, value in sorted(words_negative.iteritems(), key=lambda (k, v): (v, k), reverse=True):
+    count_negative += value
+    print "%s: %s" % (key, value)
+
+
+print "Total: postive = %s, negative = %s" % (count_positive, count_negative)

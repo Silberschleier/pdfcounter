d39b62a5d44cff363f943b93b3629eac298d8730
Christopher Schmidt (Thinkpad) <pecos@sqleb.de>
2015-05-30 15:04:29 +0200

    Added txt count

---
+++ src/txtcount.py
@@ -0,0 +1,88 @@
+import string
+import time
+
+iterations = 0
+number_words = 0
+
+txtpath = '../text_1.txt'
+
+''' Set page_begin < 0 and page_end < 0 to process whole file '''
+page_begin = -1
+page_end = -1
+
+
+''' Counting function '''
+def count_words(word, word_list):
+    global iterations
+    iterations += 1
+    s = word.lower().translate(string.maketrans("", ""), string.punctuation)
+    if word_list.get(s) is not None:
+        word_list[s] += 1
+
+words_positive = {}
+words_negative = {}
+
+''' Reading the wordlists and initialize each counter with zero '''
+file_positive = open("../wordlist_positive.txt")
+for line in file_positive:
+    words_positive[line.rstrip('\n').lower()] = 0
+print "Positive wordlist contains %s words." % len(words_positive)
+
+file_negative = open("../wordlist_negative.txt")
+for line in file_negative:
+    words_negative[line.rstrip('\n').lower()] = 0
+print "Negative wordlist contains %s words.\n" % len(words_negative)
+
+''' Load pdf and convert to strings '''
+
+print "Opening '%s'...\n\n" % txtpath
+
+''' Iterate through data '''
+
+time_start = time.time()
+
+for page in open(txtpath):
+    words = page.split(" ")
+    number_words += len(words)
+    for word in words:
+        count_words(word, words_positive)
+        count_words(word, words_negative)
+
+time_end = time.time()
+
+''' Print the results '''
+
+count_negative = 0
+count_positive = 0
+
+print "Positive:"
+print "-------------------------------"
+print "|   # |            Word | val |"
+print "-------------------------------"
+idx = 1
+for key, value in sorted(words_positive.iteritems(), key=lambda (k, v): (v, k), reverse=True):
+    if value > 0:
+        count_positive += value
+        print "| %3s | %15s | %3s |" % (idx, key, value)
+        idx += 1
+print "-------------------------------"
+
+
+print "\n\nNegative:"
+print "-------------------------------"
+print "|   # |            Word | val |"
+print "-------------------------------"
+idx = 1
+for key, value in sorted(words_negative.iteritems(), key=lambda (k, v): (v, k), reverse=True):
+    if value > 0:
+        count_negative += value
+        print "| %3s | %15s | %3s |" % (idx, key, value)
+        idx += 1
+print "-------------------------------"
+
+percent_positive = round(count_positive / float(number_words) * 100, 2)
+percent_negative = round(count_negative / float(number_words) * 100, 2)
+
+print "Total: positive = %s (%s%%), negative = %s (%s%%)\n" % (count_positive, percent_positive, count_negative, percent_negative)
+print "Processed %s words." % number_words
+print "Processing finished after %s iterations (time: %ss)" % (iterations, time_end - time_start)
